5c95352f870d8f961530d77dbd0464cbe076472a
ilievskaAna <anailievska800@gmail.com>
2023-06-08 18:32:20 +0200

    Christmas cards

---
+++ christmas.py
@@ -0,0 +1,25 @@
+import os
+
+def make_card(name):
+    template = "template.txt"
+    output_dir = "christmasCards"
+    output_file = os.path.join(output_dir, name + ".txt")
+
+    with open(template, 'r') as template_file:
+        letter = template_file.read().replace("<name>", name)
+
+    with open(output_file, 'w') as output:
+        output.write(letter)
+
+def main():
+    names_file = "names.txt"
+    names = []
+
+    with open(names_file, 'r') as file:
+        names = [name.strip() for name in file.readlines()]
+
+    os.makedirs("christmasCards", exist_ok=True)
+
+    for name in names:
+        make_card(name)
+
